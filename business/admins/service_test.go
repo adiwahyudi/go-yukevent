3ea0d388dfc025359c159b8084e2bb83bc0ffdfa
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2021-10-08 12:45:11 +0700

    admins unit testing 80%+

--- business/admins/service_test.go
+++ business/admins/service_test.go
@@ -3,9 +3,9 @@ package admins_test
 import (
 	"testing"
 	"yukevent/app/middleware"
+	"yukevent/business"
 	"yukevent/business/admins"
 	_adminMock "yukevent/business/admins/mocks"
-	"yukevent/business/users"
 	"yukevent/helpers/encrypt"
 
 	"github.com/stretchr/testify/assert"
@@ -31,14 +31,12 @@ func TestMain(m *testing.M) {
 func TestRegister(t *testing.T) {
 	t.Run("test case 1, valid test for register", func(t *testing.T) {
 		password, _ := encrypt.HashingPassword("123456")
-		outputDomain := users.Domain{
-			ID:       1,
+		outputDomain := admins.Domain{
 			Username: "adiwahyudi",
-			Password: "123456",
+			Password: password,
 		}
-		inputService := users.Domain{
-			ID:       1,
-			Username: "iwayanadiwahyudi",
+		inputService := admins.Domain{
+			Username: "adiwahyudi",
 			Password: "123456",
 		}
 		mockAdminRepository.On("Register", mock.Anything).Return(outputDomain, nil).Once()
@@ -46,5 +44,62 @@ func TestRegister(t *testing.T) {
 		resp, err := adminService.Register(&inputService)
 		assert.Nil(t, err)
 		assert.Equal(t, inputService.Username, resp.Username)
+	})
+
+	t.Run("test case 2, invalid test for register wrong password", func(t *testing.T) {
+		password, _ := encrypt.HashingPassword("654321")
+		outputDomain := admins.Domain{
+			Username: "adiwahyudi",
+			Password: password,
+		}
+		inputService := admins.Domain{
+			Username: "adiwahyudi",
+			Password: "123456",
+		}
+		mockAdminRepository.On("Register", mock.Anything).Return(outputDomain, business.ErrInternalServer).Once()
+
+		resp, err := adminService.Register(&inputService)
+		assert.Equal(t, err, business.ErrInternalServer)
+		assert.Empty(t, resp)
+	})
+}
+
+func TestLogin(t *testing.T) {
+	t.Run("test case 1, valid test for login", func(t *testing.T) {
+		password, _ := encrypt.HashingPassword("123456")
+		outputDomain := admins.Domain{
+			Username: "adiwahyudi",
+			Password: password,
+		}
+		inputService := admins.Domain{
+			Username: "adiwahyudi",
+			Password: "123456",
+		}
+
+		mockAdminRepository.On("Login", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(outputDomain, nil).Once()
+
+		resp, err := adminService.Login(inputService.Username, inputService.Password)
+
+		assert.Nil(t, err)
+		assert.NotEmpty(t, resp)
+	})
+
+	t.Run("test case 2, invalid test for login wrong password", func(t *testing.T) {
+		password, _ := encrypt.HashingPassword("654321")
+		outputDomain := admins.Domain{
+			Username: "adiwahyudi",
+			Password: password,
+		}
+		inputService := admins.Domain{
+			Username: "adiwahyudi",
+			Password: "123456",
+		}
+		mockAdminRepository.On("Register", mock.Anything).Return(outputDomain, business.ErrInternalServer).Once()
+
+		mockAdminRepository.On("Login", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(outputDomain, business.ErrEmailorPass).Once()
+
+		resp, err := adminService.Login(inputService.Username, inputService.Password)
+		assert.Equal(t, err, business.ErrEmailorPass)
+		assert.Empty(t, resp)
 	})
 }
